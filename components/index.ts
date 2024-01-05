7e2a5665b45debd2e73b37279689094a2b544e4e
hanier <hsmp16@gmail.com>
2024-01-05 01:02:02 -0600

    add hook pathName

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,5 @@
+import { ActiveLink } from "./active-link/ActiveLink";
 export * from "./navbar/Navbar";
+
+//Client Component
+export { ActiveLink } from "./active-link/ActiveLink";
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { HomeIcon } from "@primer/octicons-react";
+import { ActiveLink } from "..";
 
 const navItems = [
-  { path: "/about", text: "About" },
-  { path: "/pricing", text: "Pricing" },
   { path: "/contact", text: "Contact" },
+  { path: "/pricing", text: "Pricing" },
+  { path: "/about", text: "About" },
 ];
 
 export const Navbar = () => {
@@ -18,20 +19,8 @@ export const Navbar = () => {
       <div className="flex flex-1"></div>
 
       {navItems.map((navItem) => (
-        <Link key={navItem.path} className="mr-2" href={navItem.path}>
-          {navItem.text}
-        </Link>
+        <ActiveLink key={navItem.path} {...navItem} />
       ))}
-
-      {/* <Link className="mr-2" href="/about">
-        About
-      </Link>
-      <Link className="mr-2" href="/pricing">
-        Pricing
-      </Link>
-      <Link className="mr-2" href="/contact">
-        Contact
-      </Link> */}
     </nav>
   );
 };
